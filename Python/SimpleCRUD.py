7ad8dd4c315749d3799acd242098e349fdac2783
Douglas Silva Santos <76598304+skyzzin@users.noreply.github.com>
2023-01-11 01:39:00 -0300

    Update SimpleCRUD.py

--- Python/SimpleCRUD.py
+++ Python/SimpleCRUD.py
@@ -1,14 +1,16 @@
+import sqlite3 
+        
 
-import sqlite3
 
 def PrintDB():
-     banco = sqlite3.connect('banco.db')
-     cursor = banco.cursor()
-     cursor.execute("SELECT * FROM users")
-     for linha in cursor.fetchall():
-        print(linha)
+    banco = sqlite3.connect('banco.db')
+    cursor = banco.cursor()
+    cursor.execute("SELECT * FROM users")
+    for linha in cursor.fetchall():
+     print(linha)
+    banco.commit()
+    banco.close()
 
-        
 def InsertDB(nome,senha):
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -25,12 +27,28 @@ def InsertDB(nome,senha):
 def SearchDB(nome,senha):
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users(nome VARCHAR,senha INT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users(nome VARCHAR,senha INT,id PRIMARY KEY)')
     nome = nome  
     senha = senha
     cursor.execute("SELECT * FROM users WHERE nome=? AND senha=? ",(nome,senha))
-    for linha in cursor.fetchall():
-        print(linha)
+    resultado = cursor.fetchall()
+    if resultado:
+        print(nome,senha)
+    else:
+        return 'erro'
+
+def UpdateDB(senha,nome):
+    banco = sqlite3.connect('banco.db')
+    cursor = banco.cursor()
+    cursor.execute('UPDATE users set senha=? WHERE nome=?',(senha,nome))
     banco.commit()
     banco.close()
+    
 
+def DeleteDB(nome,senha):
+    banco = sqlite3.connect('banco.db')
+    cursor = banco.cursor()
+    cursor.execute('DELETE FROM users WHERE nome=? AND senha=?',(nome,senha))
+    banco.commit()
+    print("Conta Deletada")
+       
